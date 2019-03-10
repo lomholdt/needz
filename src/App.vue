c334615dcc18e934ee85876792d0dc2f3b298468
lomholdt <jonaslomholdt@gmail.com>
2019-03-10 22:29:12 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/needz.png">
+    <h2>mindre besvær og mere tid i din hverdag.</h2>
+    <h3>Fra d. 1. april leverer vi abonnementer på hverdagsting.</h3>
+    <p>Til din baby, til dit køkken, til dit badeværelse, dit kæledyr, til din sportsgren og du bestemmer selv frekvensen.
+Vil du have produktet hver uge, hver anden uge, hver fjerde, hver anden måned eller hvert halve år - det er op til dig. Prisen er den du kender fra butikken, du skal ikke betale fragt og vi leverer til din hoveddør. Hvad vil DU gerne have på abonnement?
+</p>
+    <MailChimp></MailChimp>
+  </div>
+</template>
+
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+import MailChimp from './components/MailChimp.vue'
+
+export default {
+  name: 'app',
+  components: {
+    HelloWorld,
+    MailChimp
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/MailChimp.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <!-- Begin Mailchimp Signup Form -->
+    <link
+      href="//cdn-images.mailchimp.com/embedcode/horizontal-slim-10_7.css"
+      rel="stylesheet"
+      type="text/css"
+    >
+    <div id="mc_embed_signup">
+      <form
+        action="https://needz.us20.list-manage.com/subscribe/post?u=baa77cec3065fecda87e89387&amp;id=c66ef31fc3"
+        method="post"
+        id="mc-embedded-subscribe-form"
+        name="mc-embedded-subscribe-form"
+        class="validate"
+        target="_blank"
+        novalidate
+      >
+        <div id="mc_embed_signup_scroll">
+          <input
+            type="email"
+            value
+            name="EMAIL"
+            class="email"
+            id="mce-EMAIL"
+            placeholder="Email"
+            required
+          >
+          <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+          <div style="position: absolute; left: -5000px;" aria-hidden="true">
+            <input type="text" name="b_baa77cec3065fecda87e89387_c66ef31fc3" tabindex="-1" value>
+          </div>
+          <div class="clear">
+            <input
+              type="submit"
+              value="Tilmeld"
+              name="subscribe"
+              id="mc-embedded-subscribe"
+              class="button"
+            >
+          </div>
+        </div>
+      </form>
+    </div>
+
+    <!--End mc_embed_signup-->
+  </div>
+</template>
+
+<script>
+export default {
+  name: "MailChimp"
+};
+</script>
+
+<style type="text/css">
+#mc_embed_signup {
+  background: #fff;
+  clear: left;
+  font: 14px Helvetica, Arial, sans-serif;
+  width: 100%;
+}
+.clear {
+  margin-left: 10px;
+}
+</style>
